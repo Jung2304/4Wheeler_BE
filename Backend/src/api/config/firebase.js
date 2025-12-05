15c18c5f7135a8ad4d6130285818017fedb2fd0c
JungNguyen <minhdung38109141@gmail.com>
2025-12-05 12:42:01 +0700

    Modify firebase.js file
    The old firebase.js file require a secret key file that is in gitignore (doesn't push on Github), so Render won't recognize. Now I put it in .env

--- Backend/src/api/config/firebase.js
+++ Backend/src/api/config/firebase.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../../../wheeler-32b8c-firebase-adminsdk-fbsvc-f7d358d1de.json");    // Firebase private key file
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);    // Firebase private key file
 
 //! FIREBASE ADMIN INIT
 if (!admin.apps.length) {
