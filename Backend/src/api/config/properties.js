8bd6b733e922026b447f64234d36ddc49e6b2034
JungNguyen <minhdung38109141@gmail.com>
2025-12-08 23:12:37 +0700

    Minor error
    Delete the slash

--- Backend/src/api/config/properties.js
+++ Backend/src/api/config/properties.js
@@ -1,7 +1,7 @@
 module.exports = {
   CORS : {
     origin: [
-      'https://fourwheeler-cyan.vercel.app/', // production
+      'https://fourwheeler-cyan.vercel.app', // production
       'http://localhost:5137'       // local development
     ],
     credentials: true              // Allow credentials (cookies) in cross-origin requests
