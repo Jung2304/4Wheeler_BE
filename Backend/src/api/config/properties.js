919965959cedb5515262da178be9a2819c80bc6f
JungNguyen <minhdung38109141@gmail.com>
2025-12-09 17:08:10 +0700

    Update CORS for admin panel
    For admin page

--- Backend/src/api/config/properties.js
+++ Backend/src/api/config/properties.js
@@ -2,7 +2,9 @@ module.exports = {
   CORS : {
     origin: [
       'https://fourwheeler-cyan.vercel.app', // production
-      'http://localhost:5137'       // local development
+      'http://localhost:5137',       // local development
+      'https://4wheeleradmin.vercel.app/' // admin panel
+      
     ],
     credentials: true              // Allow credentials (cookies) in cross-origin requests
   }
